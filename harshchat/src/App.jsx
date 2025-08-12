b568c9a316fa8aef19820f53ccade189bb9da275
harsh <harshkumarjha2610@gmail.com>
2025-08-12 18:34:48 +0530

    finalpush

--- harshchat/src/App.jsx
+++ harshchat/src/App.jsx
@@ -1,12 +1,12 @@
 // Import all images at the top
-import image1 from '/images/image1.jpg';
-import image2 from '/images/image2.jpg';
-import image3 from '/images/image3.jpg';
-import image4 from '/images/image4.png';
-import image5 from '/images/image5.jpg';
-import image6 from '/images/image6.jpg';
-import image7 from '/images/image7.jpg';
-import image8 from '/images/image8.jpg';
+import image1 from './assets/images/image1.jpg';
+import image2 from './assets/images/image2.jpg';
+import image3 from './assets/images/image3.jpg'
+import image4 from './assets/images/image4.png';
+import image5 from './images/image5.jpg';
+import image6 from './images/image6.jpg';
+import image7 from './images/image7.jpg';
+import image8 from './images/image8.jpg';
 
 // Replace the friendImages array
 
